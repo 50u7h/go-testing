3378d6a90be6ffc31b9beae7e0d42fcb1cc6355b
50u7h <50u7h@protonmail.com>
2023-10-12 10:53:57 +0300

    Adding session to App config, and creating a SessionManager

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/alexedwards/scs/v2"
 	"log"
 	"net/http"
 )
 
-type application struct{}
+type application struct {
+	Session *scs.SessionManager
+}
 
 func main() {
 	// set up an app config
@@ -13,6 +16,9 @@ func main() {
 
 	// get application routes
 	mux := app.routes()
+
+	// get a session manager
+	app.Session = getSession()
 
 	// print out a message
 	log.Println("Starting server on port 8080...")
