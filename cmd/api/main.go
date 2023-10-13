a514347153d8cf0bc5eedd58f5c3a9b201c81873
50u7h <50u7h@protonmail.com>
2023-10-13 10:05:47 +0300

    Setting up an api with our existing code base

---
+++ cmd/api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"goTesting/pkg/repository"
+	"goTesting/pkg/repository/dbrepo"
+	"log"
+	"net/http"
+)
+
+const port = 8090
+
+type application struct {
+	DSN       string
+	DB        repository.DatabaseRepo
+	Domain    string
+	JWTSecret string
+}
+
+func main() {
+	var app application
+	flag.StringVar(&app.Domain, "domain", "example.com", "Domain for application, e.g. company.com")
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=Aa123456 dbname=users sslmode=disable timezone=UTC connect_timeout=5", "Posgtres connection")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160", "signing secret")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(conn *sql.DB) {
+		err := conn.Close()
+		if err != nil {
+
+		}
+	}(conn)
+
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+
+	log.Printf("Starting api on port %d\n", port)
+
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ cmd/api/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// register middleware
+	mux.Use(middleware.Recoverer)
+	//mux.Use(app.enableCORS)
+
+	// authentication routes - auth handler, refresh
+
+	// protected routes
+	
+	return mux
+}
