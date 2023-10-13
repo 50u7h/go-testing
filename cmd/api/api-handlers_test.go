b9428b74d19be742ebafba0dfeea948e57250995
50u7h <50u7h@protonmail.com>
2023-10-13 15:19:37 +0300

    Testing logging users out of our SPA

--- cmd/api/api-handlers_test.go
+++ cmd/api/api-handlers_test.go
@@ -244,3 +244,28 @@ func Test_app_refreshUsingCookie(t *testing.T) {
 		}
 	}
 }
+
+func Test_app_deleteRefreshCookie(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/logout", nil)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(app.deleteRefreshCookie)
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusAccepted {
+		t.Errorf("wrong status; expected %d but got %d", http.StatusAccepted, rr.Code)
+	}
+
+	foundCookie := false
+	for _, c := range rr.Result().Cookies() {
+		if c.Name == "__Host-refresh_token" {
+			foundCookie = true
+			if c.Expires.After(time.Now()) {
+				t.Errorf("cookie expiration in future, and should not be: %v", c.Expires.UTC())
+			}
+		}
+	}
+
+	if !foundCookie {
+		t.Error("__Host-refresh_token cookie not found")
+	}
+}
