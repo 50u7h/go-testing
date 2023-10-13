c13082c32931bf1bbc69a6db413b25bcf80c19d1
50u7h <50u7h@protonmail.com>
2023-10-13 10:34:00 +0300

    Adding stub handlers (endpoints) for API

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,8 +15,19 @@ func (app *application) routes() http.Handler {
 	//mux.Use(app.enableCORS)
 
 	// authentication routes - auth handler, refresh
+	mux.Post("/auth", app.authenticate)
+	mux.Post("/refresh-token", app.refresh)
 
 	// protected routes
-	
+	mux.Route("/users", func(mux chi.Router) {
+		//mux.Use(app.authRequired)
+
+		mux.Get("/", app.allUsers)
+		mux.Get("/{userID}", app.getUser)
+		mux.Delete("/{userID}", app.deleteUser)
+		mux.Put("/", app.insertUser)
+		mux.Patch("/", app.updateUser)
+	})
+
 	return mux
 }
